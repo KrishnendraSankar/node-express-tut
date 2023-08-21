440ff7928f665fd578a880339e7e62e69f7345a0
Krishnendra <krish.ksg@gmail.com>
2023-08-21 11:07:58 +0530

    api changes

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const cors = require("cors");
+
 const app = express();
-const port = 3000;
+const port = 3333;
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
@@ -10,16 +14,19 @@ app.use(express.json());
 
 let prod = [
   {
+    uuid: 1,
     name: "Product1",
     description: "Description of Product 1",
     Price: "200",
   },
   {
+    uuid: 2,
     name: "Product2",
     description: "Description of Product 2",
     Price: "300",
   },
   {
+    uuid: 3,
     name: "Product3",
     description: "Description of Product 3",
     Price: "200",
@@ -44,6 +51,8 @@ app.get("/product_price/:price", function (req, res) {
 
 app.post("/product_add", function (req, res) {
   //console.log(req.body);
-  prod.push(req.body);
+  const uuid = uuidv4();
+  prod.push({ ...req.body, uuid });
+  //prod.push(req.body);
   res.json(prod);
 });
