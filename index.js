68c165fea728cc6bc3a69440bde2562060500898
Krishnendra <krish.ksg@gmail.com>
2023-08-27 11:12:13 +0530

    db function add api

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET_KEY = "secret1";
 
 const app = express();
 const port = 3333;
+
+//POSTGRES CONNECTION SETTINGS START//
+const pool = require("pg").Pool;
+const db = new pool({
+  host: "localhost",
+  port: 5432,
+  user: "postgres",
+  password: "postgres",
+  database: "testapi",
+});
+//POSTGRES CONNECTION SETTINGS END//
+
 app.use(cors());
 
 app.listen(port, () => {
@@ -12,24 +26,17 @@ app.listen(port, () => {
 
 app.use(express.json());
 
-let prod = [
+let prod = [];
+let user = [
   {
-    uuid: 1,
-    name: "Product1",
-    description: "Description of Product 1",
-    Price: "200",
+    id: 1,
+    username: "user1",
+    password: "password1",
   },
   {
-    uuid: 2,
-    name: "Product2",
-    description: "Description of Product 2",
-    Price: "300",
-  },
-  {
-    uuid: 3,
-    name: "Product3",
-    description: "Description of Product 3",
-    Price: "200",
+    id: 2,
+    username: "user2",
+    password: "password2",
   },
 ];
 
@@ -55,4 +62,23 @@ app.post("/product_add", function (req, res) {
   prod.push({ ...req.body, uuid });
   //prod.push(req.body);
   res.json(prod);
+});
+
+app.post("/login", async function (req, res) {
+  const { username, password } = req.body;
+  let result = await db.query(`SELECT * from users where username = '${username}'`);
+  console.log(result.rows);
+  if (!result.rows[0].username) {
+    return res.status(400).json({ statusMsg: false, error: "Please try to login with correct credentials" });
+  } else {
+    if (result.rows[0].password !== password) {
+      return res.status(400).json({ statusMsg: false, error: "Password Incorrect" });
+    } else {
+      var data = {
+        username: result.rows[0].username,
+      };
+      let token_str = await jwt.sign(data, JWT_SECRET_KEY);
+      return res.status(200).json({ statusMsg: true, message: "User Logged In Successfully", token: token_str });
+    }
+  }
 });
